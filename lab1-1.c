b4a63e4607d43e7180155dedce16d581c7ea23d2
atharva-potdar <atharvapotdar07@gmail.com>
2025-09-29 07:12:26 +0530

    feat: initial release

---
+++ lab1-1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* Lab 1: 2025-09-29
+ * 1. Write a C program to declare variables of different data types (int,
+ *    float, char, double) and print their values and sizes using sizeof().
+ *    Find the precision in case of float and double. Consider the following
+ *    variables where a = 10, b = 5.1234567890, c = 'G',
+ *    d = 123.1234567891234567890.
+ */
+
+
+/*
+int main(void)
+{
+    int a = 10;
+    float b = 5.1234567890;
+    char c = 'G';
+    double d = 123.1234567891234567890;
+
+    printf("Integer: %d, Size: %d bytes\n", a, sizeof(a));
+    printf("Float: %.9f, Size: %d bytes\n", b, sizeof(b));
+    printf("Char: %c, Size: %d bytes\n", c, sizeof(c));
+    printf("Double: %.16lf, Size: %d bytes\n", d, sizeof(d));
+
+    // Looking at the output, we can tell that float was accurate upto 6
+    // decimal points here. Similarly, double was accurate upto 14 decimal
+    // points here.
+
+    return 0;
+}
+*/
+
---
+++ lab1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Lab 1: 2025-09-29
+ * 2.
+ */
+
+int main(void)
+{
+
+    return 0;
+}
