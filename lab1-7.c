7ad9733502d9b8d4ff829df20b3790738bd21874
atharva-potdar <atharvapotdar07@gmail.com>
2025-09-29 20:17:53 +0530

    feat: solve problems 7 and 8

---
+++ lab1-7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Lab 1: 2025-09-29 [HOME]
+ * 7. Write a C program to demonstrate increment (++) and decrement (--)
+ *    operators in both prefix and postfix forms. Start with a variable
+ *    'a' initialized to value 5.
+ */
+
+
+int main(void)
+{
+    int a = 5;
+
+    printf("Original value: %d\n", a);
+    printf("Post-increment: %d\n", a++);
+    printf("After post-increment: %d\n", a);
+    printf("Pre-increment: %d\n", ++a);
+    printf("Post-decrement: %d\n", a--);
+    printf("After post-decrement: %d\n", a);
+    printf("Pre-decrement: %d\n", --a);
+
+    return 0;
+}
---
+++ lab1-8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* Lab 1: 2025-09-29 [HOME]
+ * 8. Write a C program to input a number and check whether it is even or odd
+ *    using the conditional operator (?:).
+ */
+
+
+int main(void)
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf((num % 2 == 0) ? "Even\n" : "Odd\n");
+
+    return 0;
+}
